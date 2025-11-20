ac3575b61b9d3606b231abce6b8ebecaed89ada2
Hannah <hannah.xia@ucalgary.ca>
2025-11-20 11:22:28 -0700

    wrote some of MainMenu

--- src/main/java/launcher/MainMenu.java
+++ src/main/java/launcher/MainMenu.java
@@ -1,4 +1,31 @@
 package launcher;
 
+import java.util.Scanner;
+
 public class MainMenu {
+    //want to use a switch statement for menu options
+    //the user can choose to run different coding challenges
+
+    public static void main(String[] args) {
+        //create a scanner
+        Scanner scanner = new Scanner(System.in);
+
+        int choice; //a variable that holds the choice chosen by the user
+
+        //impliment standard menu loop
+        while (true) {
+            System.out.println("Coding Challenges Menu");
+
+
+            choice = scanner.nextInt(); //choice will be set to the next inputted number
+
+            switch (choice) {
+
+
+                case 0:
+                    System.out.println("Exiting program. bye bye!");
+                    return;
+            }
+        }
+    }
 }
