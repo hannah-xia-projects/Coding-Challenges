605eff05f1a2e1f7fabc9594da8ad99c7b4ebb92
Hannah <hannah.xia@ucalgary.ca>
2025-11-26 16:12:38 -0700

    added and wrote a README file for snake

---
+++ javascript/challenges/games/snake-game/README.md
@@ -0,0 +1,7 @@
+# Snake Game
+A standard snake game where the player plays as a snake with a short length. Around the map, there will be apple that will randomly spawn. The player's objective is to eat all apples which in turn grows the snake's length. If the player runs into themselves, they lose!
+
+## Files included:
+- index.html
+- script.js
+- style.css 
--- javascript/challenges/test.js
+++ javascript/challenges/test.js
@@ -2,7 +2,7 @@ console.log("JavaScript challenges are ready!");
 
 //test reversing of strings just to see how javascript works!
 function reverseString(str) {
-    const reversed = str.split('').reverse().join(''); //create a const variable for the reverse of the string
+    const reversed = str.split('').reverse().join(''); //create a const variable for the reverse of the s
     return reversed; 
 }
 
