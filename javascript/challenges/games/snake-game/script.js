53d8a103941c323dc194aeb2024c908de2b881c0
Hannah <hannah.xia@ucalgary.ca>
2025-11-27 13:44:16 -0700

    create const variables in script.js

--- javascript/challenges/games/snake-game/script.js
+++ javascript/challenges/games/snake-game/script.js
@@ -1,1 +1,6 @@
 //make a quick snake game in javascript to learn it!
+const canvas = document.getElementById('gameCanvas');
+const start = document.getElementById('start');
+const pause = document.getElementById('pause');
+const score = document.getElementById('score');
+const highScore = document.getElementById('highscore');
