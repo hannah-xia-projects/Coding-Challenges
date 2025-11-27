05cbc5d1ab7fadcb1d2535aacbfcfec49cb7152d
Hannah <hannah.xia@ucalgary.ca>
2025-11-27 11:04:08 -0700

    commented some rules of javascript in test.js for future use

--- javascript/challenges/test.js
+++ javascript/challenges/test.js
@@ -1,7 +1,12 @@
 console.log("JavaScript challenges are ready!");
 
 //test reversing of strings just to see how javascript works!
+//this is how you create a function in javascript
 function reverseString(str) {
+    //instead of variable types like in Java, 
+    //we use const for a variable that will not change and block scoped
+    //let: Block scoped, cannot be re-declared within the same scope, but can be re-assigned.
+    //var: Function-scoped or globally-scoped, can be redeclared or re-assigned
     const reversed = str.split('').reverse().join(''); //create a const variable for the reverse of the s
     return reversed; 
 }
